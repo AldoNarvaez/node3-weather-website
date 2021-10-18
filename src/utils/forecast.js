50f55622b100a8dcc133ee6a79acbef1382afc68
Aldo Narvaez Cao Romero <anarvaez@intekglobal.com>
2021-10-18 16:47:51 -0500

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast=(latitude,longitude,callback)=>{
  			callback("Unable to find location",undefined)
 
  		}else{
- 			callback(undefined,body.current.weather_descriptions[0]+". it is currently "+ body.current.temperature+" degrees out. There is an apparent temperature of "+ body.current.feelslike+" degrees out")
+ 			callback(undefined,body.current.weather_descriptions[0]+". it is currently "+ body.current.temperature+" degrees out. There is an apparent temperature of "+ body.current.feelslike+" degrees out. With a humidity of "+body.current.humidity+"%")
  		}
  	})
 }
